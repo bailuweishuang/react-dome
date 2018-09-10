5f5d9c180e0cc8bf49f059237d4fdf78590befb0
huyongxiang@zdsoft.cn <huyongxiang@zdsoft.cn>
2018-09-10 16:02:43 +0800

    测试

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 class App extends React.Component {
   render() {
-    return <div>Hello s World</div>;
+    return <div>Hello World</div>;
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
 var webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
 module.exports = {
   entry: "./src/app.js",
-  output: { path: __dirname + "/build", filename: "bundle.js" },
+  output: {
+    path: path.join(__dirname + "/build"),
+    publicPath: "/",
+    filename: "bundle.js",
+    chunkFilename: "js/[name]-[hash]" + ".js"
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./index.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      },
+      filename: "index.html"
+    })
+  ],
   module: {
     rules: [
       {
@@ -13,7 +32,54 @@ module.exports = {
           presets: ["es2015", "react", "stage-2"]
         }
       },
-      { test: /\.css$/, loader: "style-loader!css-loader" }
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "static/img/[name].[hash:7].[ext]"
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "static/media/[name].[hash:7].[ext]"
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "static/fonts/[name].[hash:7].[ext]"
+        }
+      }
     ]
-  }
+  },
+  devServer: {
+    // contentBase: path.join(__dirname, ""),
+    contentBase: false, //since we use CopyWebpackPlugin.
+    clientLogLevel: 'warning',
+    publicPath: '/',
+    hot: true,
+    progress: true,
+    overlay: { warnings: false, errors: true },
+    historyApiFallback: {
+        rewrites: [
+            { from: /.*/, to: path.posix.join('/', 'index.html') },
+        ],
+    },
+    // historyApiFallback: true,
+    // quiet: true, // necessary for FriendlyErrorsPlugin
+    compress: true,
+    inline: true,
+    port: 8083,
+    host: '127.0.0.1',
+    watchOptions: {
+        poll: false,
+    }
+}
 };
