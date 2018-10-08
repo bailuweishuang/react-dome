5d0cdebc9845b7e1903ad08f33dd65201a231b8e
huyongxiang@zdsoft.cn <huyongxiang@zdsoft.cn>
2018-10-08 15:37:21 +0800

    cs

---
+++ src/pages/kuayu/index.jsx
@@ -0,0 +1,47 @@
+class Kuayu extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  /*JSONP跨域
+    原理： 利用<script> 标签没有跨域限制的漏洞 通过<script>标签指向一个需要访问的地址并提供一个回调函数来接受数据当需求通讯
+    <script src="http://domani/api?param1=a&callback=jsonp"></script>
+    <script>
+     function jsonp (data) {
+         console.log(data)
+     }
+    </script>
+    (简单 兼容性不错 但是只限于get请求)*/
+    //封装自己的JSONP
+    jsonp = (url, jsonpcallback, success) => {
+        let script = document.createElement("script");
+        script.url = url;
+        script.async = true;
+        script.type = "text/javascript";
+        window[jsonpcallback] = data = () => {
+            success && success(data)
+        };
+        document.body.appendChild(script) 
+    }
+    // jsonp(
+        "http://xxx";
+        "callback";
+        function(value){
+            console.log(value)
+        }
+    // )
+    /*
+    CORS
+    CORS背后的思想，就是使用自定义的HTTP头部让浏览器与服务器进行沟通，从而决定请求或响应是应该成功，还是应该失败
+    IE: var xdr = new XDomainRequest();
+        xdr.onload = function () {
+            console.log(xdr.responseText);
+        }
+        xdr.open('get', 'http://www.baidu.com.cn');
+        ...
+        xdr.send(null)
+     */
+  render() {
+    return "";
+  }
+}
--- src/pages/mine/index.jsx
+++ src/pages/mine/index.jsx
@@ -6,14 +6,92 @@ class Home extends React.Component {
     this.state = {};
   }
   componentWillMount() {}
-  componentDidMount() {}
+  componentDidMount() {
+    //适合用事件委托的事件：click，mousedown，mouseup，keydown，keyup，keypress
+    window.onload = () => {
+      let ul = document.getElementById("ul");
+      let btn = document.getElementById("btn");
+      let mun = 4;
+
+      ul.addEventListener("click", e => {
+        let ev = e || window.event;
+        let target = ev.target || ev.srcElement;
+        if (target.nodeName.toLowerCase() == "li") {
+          switch (target.id) {
+            case "one":
+              console.log(1);
+              break;
+            case "two":
+              console.log(2);
+              break;
+            default:
+              console.log(3);
+          }
+        }
+      });
+      ul.addEventListener("mouseover", e => {
+        let ev = e || window.event;
+        let target = ev.target || ev.srcElement;
+        if (target.nodeName.toLowerCase() == "li") {
+          switch (target.id) {
+            case "one":
+              target.style.background = "yellow";
+              break;
+            case "two":
+              target.style.background = "red";
+              break;
+            default:
+              target.style.background = "blue";
+          }
+        }
+      });
+      ul.addEventListener("mouseout", e => {
+        let ev = e || window.event;
+        let target = ev.target || ev.srcElement;
+        if (target.nodeName.toLowerCase() == "li") {
+          target.style.background = "#ccc";
+        }
+      });
+      btn.addEventListener("click", () => {
+        mun++;
+        let a = document.createElement("li");
+        a.innerHTML =
+          "<input id='input'></input>" + '<span id="delete">删除</span>';
+        ul.appendChild(a);
+        let arr = document.querySelectorAll("#input");
+        [...arr].map((item, index) => {
+          item.value = index
+        })
+      });
+      ul.addEventListener("click", e => {
+        let ev = e || e.event;
+        let target = ev.target || ev.srcElement;
+        if (target.id.toLowerCase() == "delete") {
+          target.parentNode.parentNode.removeChild(target.parentNode);
+        }
+      });
+    };
+  }
   render() {
     return (
-      <div className="mine" onClick={()=>{
-          this.props.history.push("/")
-          }}>
+      <div className="mine">
         我的
         <p className="mine-title">想和你哭 想和你笑 想拥你入我怀抱 这是一个传说</p>
+        <ul id="ul">
+          <li id="one">
+            1<span id="delete">删除</span>
+          </li>
+          <li id="two">
+            2<span id="delete">删除</span>
+          </li>
+          <li id="three">
+            3<span id="delete">删除</span>
+          </li>
+          <li in="four">
+            4<span id="delete">删除</span>
+          </li>
+        </ul>
+        <button id="btn">添加</button>
       </div>
     );
   }
